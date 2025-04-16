599dfdd4158752d48f29620e78519fc24bbd3119
leeuihyun <meta212121@naver.com>
2025-04-16 21:35:09 +0900

    feat : 사칙연산 Class (Generic 이용) Generic T type을 이용한 사칙연산 핵심 기능 구현

---
+++ src/ArithmeticCalculator.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArithmeticCalculator<T extends Number> {
+
+  private T firstNumber;
+  private T secondNumber;
+  private char sign;
+  private List<T> resultList;
+
+  public void setFirstNumber(T firstNumber) {
+    this.firstNumber = firstNumber;
+  }
+
+  public void setSecondNumber(T secondNumber) {
+    this.secondNumber = secondNumber;
+  }
+
+  public void setSign(char sign) {
+    this.sign = sign;
+  }
+
+  public void calculate() throws Exception {
+    if (resultList == null) {
+      resultList = new ArrayList<>();
+    }
+
+    double result = switch (this.sign) {
+      case '+' -> firstNumber.doubleValue() + secondNumber.doubleValue();
+      case '-' -> firstNumber.doubleValue() - secondNumber.doubleValue();
+      case '*' -> firstNumber.doubleValue() * secondNumber.doubleValue();
+      case '/' -> firstNumber.doubleValue() / secondNumber.doubleValue();
+      default -> throw new IllegalStateException("존재하지 않는 연산자입니다.");
+    };
+
+    if (this.firstNumber instanceof Double | this.secondNumber instanceof Double) {
+      resultList.add((T) Double.valueOf(result));
+    } else {
+      resultList.add((T) Integer.valueOf((int) result));
+    }
+  }
+
+  public void printResult() {
+    System.out.println("----결과 출력 시작----");
+    for (Number result : resultList) {
+      System.out.print(result + " ");
+    }
+    System.out.println();
+    System.out.println("----결과 출력 종료----");
+  }
+}
