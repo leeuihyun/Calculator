1d1d8eead283d272eb80023cb3a9c21980d45974
leeuihyun <meta212121@naver.com>
2025-04-18 11:46:38 +0900

    refactor : 중복 로직 수정 Main 내부의 파싱 메서드와 계산기 클래스에서의 형변환이 중복되어 변경

--- src/ArithmeticCalculator.java
+++ src/ArithmeticCalculator.java
@@ -26,22 +26,23 @@ public class ArithmeticCalculator<T extends Number> {
     }
 
     double result = switch (this.sign) {
-      case '+' -> firstNumber.doubleValue() + secondNumber.doubleValue();
-      case '-' -> firstNumber.doubleValue() - secondNumber.doubleValue();
-      case '*' -> firstNumber.doubleValue() * secondNumber.doubleValue();
+      case '+' -> (Double) firstNumber + (Double) secondNumber;
+      case '-' -> (Double) firstNumber - (Double) secondNumber;
+      case '*' -> (Double) firstNumber * (Double) secondNumber;
       case '/' -> {
-        if (secondNumber.doubleValue() == 0.0) {
+        if ((Double) secondNumber == 0.0) {
           throw new Exception("나누기 연산은 두번째 숫자가 0이 될 수 없습니다. 재시작합니다.");
         }
-        yield firstNumber.doubleValue() / secondNumber.doubleValue();
+        yield (Double) firstNumber / (Double) secondNumber;
       }
       default -> throw new Exception("존재하지 않는 연산자입니다.");
     };
 
-    if (this.firstNumber instanceof Double | this.secondNumber instanceof Double) {
-      resultList.add((T) Double.valueOf(result));
-    } else {
+    if (isInteger(this.firstNumber) && isInteger(this.secondNumber)) {
       resultList.add((T) Integer.valueOf((int) result));
+    } else {
+      double doubleResult = Math.round(result * 100.0) / 100.0;
+      resultList.add((T) Double.valueOf(doubleResult));
     }
   }
 
@@ -57,5 +58,10 @@ public class ArithmeticCalculator<T extends Number> {
     }
     System.out.println();
     System.out.println("----결과 출력 종료----");
+  }
+
+  public boolean isInteger(T number) {
+    System.out.println(number.doubleValue() % 1);
+    return number.doubleValue() % 1 == 0.0;
   }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,19 @@ import java.util.Scanner;
 
 public class Main {
 
-  static Number parseNumber(String str) throws Exception {
-    Number number = str.contains(".") ? Double.parseDouble(str) : Integer.parseInt(str);
-
-    if (number.doubleValue() < 0) {
-      throw new Exception("입력하신 숫자가 잘못되었습니다. 0 이상인 정수 또는 실수만 입력해주세요.");
-    }
-
-    return number;
-  }
-
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    ArithmeticCalculator<Number> arithmeticCalculator = new ArithmeticCalculator<>();
+    ArithmeticCalculator<Double> arithmeticCalculator = new ArithmeticCalculator<>();
 
     while (true) {
       System.out.println("----계산기 실행----");
 
       System.out.print("첫번째 양의 정수 입력 : ");
       String firstNumberStr = scan.next();
-      Number firstNumber;
+      double firstNumber;
 
       try {
-        firstNumber = parseNumber(firstNumberStr);
+        firstNumber = Double.parseDouble(firstNumberStr);
       } catch (Exception e) {
         System.out.println(e.getMessage());
         continue;
@@ -33,10 +23,10 @@ public class Main {
 
       System.out.print("두번째 양의 정수 입력 : ");
       String secondNumberStr = scan.next();
-      Number secondNumber;
+      double secondNumber;
 
       try {
-        secondNumber = parseNumber(secondNumberStr);
+        secondNumber = Double.parseDouble(secondNumberStr);
       } catch (Exception e) {
         System.out.println(e.getMessage());
         continue;
@@ -63,10 +53,10 @@ public class Main {
 
       System.out.print("입력받은 값보다 큰 결과값 들을 출력하겠습니다. 값을 입력하세요 : ");
       String standardNumberStr = scan.next();
-      Number standardNumber = 0;
+      double standardNumber;
 
       try {
-        standardNumber = parseNumber(standardNumberStr);
+        standardNumber = Double.parseDouble(standardNumberStr);
       } catch (Exception e) {
         System.out.println(e.getMessage());
         continue;
